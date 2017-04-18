0ac487edf60dec33de0679725a862fc91e1315a4
THEtheChad <chad.d.elliott@gmail.com>
2017-04-17 21:31:53 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,88 @@
+'use strict';
+
+const p = require('path');
+const crypto = require('crypto');
+const fs = require('fs');
+const HOME = require('os').homedir();
+
+class TokenStore { 
+
+	constructor (options) {
+
+		this.opts = Object.assign({
+			mkdir: true,
+			signature: [],
+			filename: 'token',
+			ext: 'json',
+			path: '$HOME/.credentials'
+		}, options);
+
+		// bash expansion
+		let path = this.opts.path.replace(/^~|^\$HOME/, HOME);
+
+		// check if directory exists
+		let exists = fs.existsSync(path);
+
+		if(!exists){
+			if(this.opts.mkdir){
+				fs.mkdirSync( path );
+			}
+			else{
+				throw new Error( path + ' does not exist' );
+			}
+		}
+
+		let filename = [this.opts.filename];
+
+		if( this.opts.signature.length ){
+			let signature = crypto.createHash('md5').update( this.opts.signature.join() ).digest('hex');
+			filename.push(signature);
+		}
+
+		filename = `${ filename.join('-') }.${ this.opts.ext }`; 
+
+		this.path = p.resolve( path, filename );
+	}
+
+	get () {
+
+		let contents;
+
+		try {
+			contents = fs.readFileSync( this.path );
+		}
+		catch(e){
+			if(e.code === 'ENOENT'){
+				throw new Error('token has not been stored');
+			}
+		}
+
+		if(this.opts.ext == 'json'){
+
+			try{
+				contents = JSON.parse(contents);
+			}
+			catch(e){
+				throw new Error('token is not stored as valid json');
+			}
+		}
+	}
+
+	store (token) {
+
+		if( typeof token !== 'string' ){
+
+			if(this.opts.ext == 'json'){
+
+				token = JSON.stringify(token);
+			}
+			else{
+				throw new Error('token must be a string');
+			}
+		}
+
+		fs.writeFileSync( this.path, token );
+	}
+}
+
+module.exports = TokenStore;
---
+++ test/test.js
@@ -0,0 +1,134 @@
+const assert = require('assert');
+const path = require('path');
+const Tstore = require('../');
+const mock = require('mock-fs');
+const fs = require('fs');
+
+const HOME = require('os').homedir();
+const CREDENTIALS = HOME + '/.credentials';
+
+mock({
+  [ HOME ]: {},
+  [ HOME + '/.credentials' ]: {
+  	'invalid.json': 'sjdfjdf',
+  	'valid.json': '{"valid":true}'
+  }
+});
+
+describe('TokenStore', function(){
+
+	describe('#constructor', function(){
+
+		it('should expand tilde', function(){
+
+			let tstore = new Tstore({
+				path: '~/.credentials',
+			});
+
+			let match = new RegExp('^' + HOME);
+
+			assert.ok( match.test( tstore.path ) );
+		});
+
+		it('should expand $HOME', function(){
+
+			let tstore = new Tstore({
+				path: '$HOME/.credentials',
+			});
+
+			let match = new RegExp('^' + HOME);
+
+			assert.ok( match.test( tstore.path ) );
+		});
+
+		it('should throw an error when passed {mkdir:false} alongside a missing path', function(){
+
+			assert.throws(function(){
+
+				let tstore = new Tstore({
+					path: '$HOME/.bleep',
+					mkdir: false
+				});
+			});
+		});
+
+		it('should create a directory when path is missing', function(){
+
+			let tstore = new Tstore({
+				path: '$HOME/.bleep'
+			});
+
+			assert.ok( fs.existsSync(HOME + '/.bleep') );
+		});
+	});
+
+	describe('#get', function(){
+
+		it('should throw an error if a token does not exist', function(){
+
+			let tstore = new Tstore({
+				filename: 'nonexistent',
+				path: '$HOME/.credentials'
+			});
+
+			assert.throws(function(){
+
+				tstore.get();
+			});
+		});
+
+
+		it('should throw an error if set to json and unable to parse token', function(){
+
+			let tstore = new Tstore({
+				filename: 'invalid',
+				path: '$HOME/.credentials'
+			});
+
+			assert.throws(function(){
+
+				tstore.get();
+			});
+		});
+
+		it('should not throw an error if token is valid json', function(){
+
+			let tstore = new Tstore({
+				filename: 'valid',
+				path: '$HOME/.credentials'
+			});
+
+			tstore.get();
+		});
+
+	});
+
+	describe('#store', function(){
+
+		it('should store any valid string', function(){
+
+			let token = 'test';
+
+			let tstore = new Tstore();
+
+			tstore.store(token);
+
+			let result = fs.readFileSync( CREDENTIALS + '/token.json', 'UTF8' );
+
+			assert.equal(token, result);
+		});
+
+		it('should stringify json by default', function(){
+
+			let token = {test:true};
+
+			let tstore = new Tstore();
+
+			tstore.store(token);
+
+			let result = fs.readFileSync( CREDENTIALS + '/token.json', 'UTF8' );
+
+			assert.deepEqual(token, JSON.parse(result));
+		});
+	});
+});
